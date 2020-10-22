15e356e55802c82f32562ade68ee9b508b3bb2af
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-10-22 20:23:30 +0300

    MyTimer Class added

---
+++ src/main/scala/MyTimer.scala
@@ -0,0 +1,15 @@
+class MyTimer {
+  var startTime:Long = 0
+  var durationTime = 0.0
+  def start()= {
+    startTime = System.nanoTime
+    startTime
+  }
+
+  def stop() = {
+    durationTime = (System.nanoTime - startTime) / 1e9d
+    durationTime
+  }
+
+  def prettyPrint(): Unit = println(s"Program run time: $durationTime seconds")
+}
