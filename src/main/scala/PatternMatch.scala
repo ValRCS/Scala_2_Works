8a29e74da50ade3beca9b44b40bcfd0623dd19d5
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-10-24 11:39:28 +0300

    Added some aliases to imports

--- src/main/scala/PatternMatch.scala
+++ src/main/scala/PatternMatch.scala
@@ -1,4 +1,6 @@
-class PatternMatch {
+//since we are not going to be storing data, just methods, it makes sense that
+//this will be an object - a single instance of the class - a singleton object
+object PatternMatch {
   def dayOfWeek(x: Int): String = x match {
     case 1 => "Mon"
     case 2 => "Tue"
@@ -10,5 +12,15 @@ class PatternMatch {
     case _ => "Unknown"
   }
 
+  def indexOfDay(d: String): Int = d match {
+    case "Mon" => 1; case "Tue" => 2
+    case "Wed" => 3; case "Thu" => 4
+    case "Fri" => 5; case "Sat" => 6
+    case "Sun" => 7; case _ => -1
+  }
+
+
+
   def printDay(x: Int): Unit = println(dayOfWeek(x))
+  def printIndex(d: String): Unit = println(indexOfDay(d))
 }
--- src/main/scala/UsingPatternMatch.scala
+++ src/main/scala/UsingPatternMatch.scala
@@ -1,5 +1,15 @@
+import PatternMatch.printIndex
+// https://alvinalexander.com/scala/how-to-rename-members-import-scala-classes-methods-functions/
+import PatternMatch.{printDay => pD}
+
 object UsingPatternMatch extends App {
-  val matcher = new PatternMatch
-  matcher.printDay(4)
-  matcher.printDay(70)
+
+  PatternMatch.printDay(4)
+  PatternMatch.printDay(70)
+  pD(6) // these kind of short names should be used when there is no confusion on their meaning
+
+  PatternMatch.printIndex("Saturday")
+  PatternMatch.printIndex("Sat")
+  printIndex("Sat")
+
 }
