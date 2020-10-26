69a6f8fbbf2ae07f43b995cf1b953312d7c744c8
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-10-26 21:20:04 +0200

    Added some more timing fun

--- src/main/scala/MyTimer.scala
+++ src/main/scala/MyTimer.scala
@@ -1,12 +1,12 @@
 class MyTimer {
   var startTime:Long = 0
   var durationTime = 0.0
-  def start()= {
+  def start(): Long = {
     startTime = System.nanoTime
     startTime
   }
 
-  def stop() = {
+  def stop(): Double = {
     durationTime = (System.nanoTime - startTime) / 1e9d
     durationTime
   }
@@ -14,14 +14,14 @@ class MyTimer {
   def prettyPrint(): Unit = println(s"Program run time: $durationTime seconds")
 
   //so a good way of testing so => delays the evaluation of f
-  def measureTime(f: => Unit) = {
+  def measureTime(f: => Unit): Long = {
 //    val start = System.currentTimeMillis()
     val start = System.nanoTime
     f
 //    val end = System.currentTimeMillis()
     val end = System.nanoTime
     val delta = end - start
-    println(s"Evaluation took $delta nanoseconds which is ${delta/1000000000d} seconds")
+//    println(s"Evaluation took $delta nanoseconds which is ${delta/1000000000d} seconds")
     delta
   }
   }
--- src/main/scala/TimingCreation.scala
+++ src/main/scala/TimingCreation.scala
@@ -1,20 +1,43 @@
 object TimingCreation extends App {
   val mt = new MyTimer()
-  mt.measureTime(Array(1,2,3,4))
-  mt.measureTime(5+5)
-  mt.measureTime(2.0+3.5)
-  mt.measureTime(Vector(1,2,3,5))
-  def myLoop(endVal: Int = 1000) = {
+//  mt.measureTime(Array(1,2,3,4))
+//  mt.measureTime(5+5)
+//  mt.measureTime(2.0+3.5)
+//  mt.measureTime(Vector(1,2,3,5))
+  def myLoop(endVal: Int = 1000): Unit = {
     for (i <- 1 to endVal) {
       i*500 // if operation do not lead to anything, there is a danger that JVM will optimize them away
     }
   }
-  mt.measureTime(myLoop(50000))
-  mt.measureTime(myLoop(1000000))
-  mt.measureTime(myLoop(1000000000))
+//  mt.measureTime(myLoop(50000))
+//  mt.measureTime(myLoop(1000000))
+//  mt.measureTime(myLoop(1000000000))
+//
+//  mt.measureTime(Range(1, 1000).toArray)
+//  mt.measureTime(Range(1, 1000).toList)
+//  mt.measureTime(Range(1, 1000).toSeq)
+//  mt.measureTime(Range(1, 1000).toVector)
 
-  mt.measureTime(Range(1, 1000).toArray)
-  mt.measureTime(Range(1, 1000).toList)
-  mt.measureTime(Range(1, 1000).toSeq)
-  mt.measureTime(Range(1, 1000).toVector)
+  val nrOfLoops = 1000
+  val collectionSize = 1000
+
+  var timeArray: Long = 0
+  var timeList: Long = 0
+  var timeSeq: Long = 0
+  var timeVector: Long = 0
+
+  for(i <- 1 to nrOfLoops) {
+//    timeArray += mt.measureTime(Range(1, collectionSize).toArray.apply(55))
+//    timeList += mt.measureTime(Range(1, collectionSize).toList.apply(55))
+//    timeSeq += mt.measureTime(Range(1, collectionSize).toSeq.apply(55))
+//    timeVector += mt.measureTime(Range(1, collectionSize).toVector.apply(55))
+    timeArray += mt.measureTime(Range(1, collectionSize).toArray)
+    timeList += mt.measureTime(Range(1, collectionSize).toList)
+    timeSeq += mt.measureTime(Range(1, collectionSize).toSeq)
+    timeVector += mt.measureTime(Range(1, collectionSize).toVector)
+  }
+
+  val myCollections = Array(timeArray, timeList, timeSeq, timeVector)
+
+  myCollections.foreach(col => println(s"Average time: ${col / nrOfLoops} nanoseconds"))
 }
