bb0f4da91aa2d514fea50259d3e8744507861848
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-10-27 20:23:16 +0200

    Trie example

--- src/main/scala/MyTrie.scala
+++ src/main/scala/MyTrie.scala
@@ -12,4 +12,23 @@ class MyTrie {
      for (c <- s if current.nonEmpty) current = current.get.children.get(c)
      current.exists(_.hasValue)
    }
+
+   def prefixesMatchingString0(s: String): Set[Int] = {
+     var current = Option(root)
+     val output = Set.newBuilder[Int]
+     for ((c, i) <- s.zipWithIndex if current.nonEmpty) {
+       if (current.get.hasValue) output += i
+       current = current.get.children.get(c)
+       }
+     if (current.exists(_.hasValue)) output += s.length
+     output.result()
+     }
+
+  def printAll() = {
+    var current = Option(root)
+    //start with printing just one random word
+    //you are guarantedd to print something because all the leaves in the Trie have x in the end(hasValues
+    //so need string builder and print whenever there is hasValue
+
+  }
 }
