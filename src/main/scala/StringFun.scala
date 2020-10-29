b949bba3a28c0492bc40cd9bd09f40be37222ad1
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-10-29 21:18:25 +0200

    Parsing some strings from text

--- src/main/scala/StringFun.scala
+++ src/main/scala/StringFun.scala
@@ -1,9 +1,16 @@
 object StringFun extends App {
   def sumNumbers(text:String) = {
     //FIXME
-    0
+//    val numPattern = "[0-9]+".r
+//    numPattern.findAllIn(text).toSeq.map(_.toInt).foldLeft(0)(_ + _)
+//    ("[0-9]+".r findAllIn text).map(_.toInt).sum
+//    ("\\d+".r findAllIn text).map(_.toInt).sum
+    ("[0-9]+".r findAllIn text).map(_.toInt).sum
+
   }
 
-  println(sumNumbers("Valdis has 3 cows and 58 sheep and 11horses")) //should print 72
+  println(sumNumbers("Valdis has 3 cows and 58 sheep and 11horses"))
+  println(sumNumbers("Valdis has 3߅५৫੫૫୫௫ cows and 58 sheep and 11horses"))
+  //should print 72
   //so pure split will not work real data is messy!
 }
