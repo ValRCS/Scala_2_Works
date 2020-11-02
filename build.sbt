bec1d4fcea063e934b28b2f9d5e8dc1e2850c952
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-11-02 20:05:58 +0200

    Logback working example without file save

--- build.sbt
+++ build.sbt
@@ -3,3 +3,8 @@ name := "Testy"
 version := "0.1"
 
 scalaVersion := "2.13.3"
+
+//you add libraries here
+libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
+//libraryDependencies += "org.clapper" %% "grizzled-slf4j" % "1.3.4"
---
+++ src/main/scala/UsingLog.scala
@@ -0,0 +1,30 @@
+//import com.typesafe.scalalogging.Logger
+//import grizzled.slf4j.Logger
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
+
+object UsingLog extends App {
+  println("Let's try some logging")
+  val expensive = 5555
+//  val logger = Logger("UsingLog") //
+//  logger.info("Hello, world")
+  val logger = Logger("myUsingLog")
+////  val logger = Logger(LoggerFactory.getLogger("mylogname"))
+  logger.debug(s"Some $expensive message!")
+  logger.info("Test")
+  //error you would log when there is an error happening
+  logger.error("Danger we got an error!")
+
+  def environmentVariables = {
+    import scala.collection.JavaConverters._
+    import scala.language.implicitConversions
+    val environmentVars = System.getenv().asScala
+    for ((k,v) <- environmentVars) println(s"key: $k, value: $v")
+
+    val properties = System.getProperties().asScala
+    for ((k,v) <- properties) println(s"key: $k, value: $v")
+  }
+  environmentVariables
+}
+
+
