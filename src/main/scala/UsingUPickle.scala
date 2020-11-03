8778a4b463be8be9f617d0c74905375df3213d97
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-11-03 21:33:58 +0200

    Reading JSON

--- src/main/scala/UsingUPickle.scala
+++ src/main/scala/UsingUPickle.scala
@@ -1,4 +1,8 @@
+import java.io.{File, PrintWriter}
+
 import upickle.default._
+
+import scala.io.Source
 
 object UsingUPickle extends App{
   println("Let's work with some JSON!")
@@ -15,5 +19,29 @@ object UsingUPickle extends App{
 
   //TODO print JSON of a list of Maps with each map containing key number->value, square->value squared and
   //cube -> number cubed
+  import upickle.default.{ReadWriter => RW, macroRW}
 
+  case class Thing(number: Int, square: Int, cube: Int)
+  object Thing{
+    implicit val rw: RW[Thing] = macroRW
+  }
+
+  val myNumbers = getNumbers(1,10)
+  for (line <- myNumbers) println(write(Thing(line(0), line(1), line(2))))
+  val myThings = for (line <- myNumbers) yield Thing(line(0), line(1), line(2))
+  println(write(myThings))
+
+//  import java.nio.file.Files
+  val pw = new PrintWriter(new File("mydata.json" ))
+  pw.write(write(myThings,indent = 2))
+  pw.close()
+
+  val fileContents = Source.fromFile("./src/resources/todo.json").getLines.mkString //so this leaves file open
+  println(fileContents)
+//  val myData = read[Seq[Map[String,String]]](fileContents) //FIXME how to read arbitrary objects
+//  println(myData.getClass)
+
+  val myJSON = Source.fromFile("./src/resources/mydata.json").getLines.mkString
+  val myData = read[Seq[Map[String,Int]]](myJSON)
+  myData.foreach(println)
 }
