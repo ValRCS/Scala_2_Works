7b9dba47e44328e335750f8b99ac06f26a814daf
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-11-03 20:05:30 +0200

    Writing CSV example

--- src/main/scala/UsingScalaCSV.scala
+++ src/main/scala/UsingScalaCSV.scala
@@ -1,11 +1,16 @@
 import java.io.File
 
-import com.github.tototoshi.csv.CSVReader
+import com.github.tototoshi.csv.{CSVReader, CSVWriter, DefaultCSVFormat}
 
 object UsingScalaCSV extends App {
   println("Going to read some files")
 //  val filePath = "./src/resources/sample.csv"
   val filePath = "./src/resources/with-headers.csv"
+
+
+
+  //so optional implicit to change formatting
+
   val reader = CSVReader.open(new File(filePath))
 //  val res = reader.all()
 //  res.foreach(line => println(line.mkString(",")))
@@ -13,4 +18,30 @@ object UsingScalaCSV extends App {
   //as with most streams you only read it once
   val resMap = reader.allWithHeaders()
   resMap.foreach(myMap => for ((k,v) <- myMap) println(s"key: $k, value: $v"))
+
+  def getNumbers(beg: Int=1, end:Int=10) = {
+    val mySeq = for (i <- beg to end) yield Seq(i, i*i, i*i*i)
+    mySeq
+  }
+  val myData = getNumbers()
+  val saveFile = "./src/resources/power-table.csv"
+
+
+
+  def saveNumbers(saveFile: String) = {
+    val writer = CSVWriter.open(new File(saveFile))
+
+    writer.writeRow(Seq("number","square","cube"))
+    writer.writeAll(getNumbers(1,10))
+
+    writer.close()
+  }
+  implicit object MyFormat extends DefaultCSVFormat {
+    override val delimiter = '#'
+  }
+
+  saveNumbers(saveFile)
+
+  saveNumbers("./src/resources/power-table2.csv")
+
 }
