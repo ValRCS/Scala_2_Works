0025d7b2cb18dfd70a74db3151974127e8f587e3
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-11-10 20:04:32 +0200

    Working Query for genres using Slick

---
+++ src/main/scala/GenSlickModel.scala
@@ -0,0 +1,24 @@
+import scala.concurrent.Await
+
+object GenSlickModel extends App {
+  println("Trying Slick")
+  import slick.jdbc.SQLiteProfile.api._ // must import
+  val db = Database.forURL(url = "c:/sqlite/db/chinook.db")
+//  lazy val db = Database.forURL(url = "c:/sqlite/db/chinook.db") with lazy the function is not called until needed
+
+//  val db = Database.forURL(url = "c:/sqlite/db/chinookbad.db")
+//  import scala.concurrent.ExecutionContext.Implicits.global
+
+  //so this code generator should run whenever we change our Database schema
+  slick.codegen.SourceCodeGenerator.main(
+    Array(
+      "slick.jdbc.SQLiteProfile",
+      "org.sqlite.JDBC",
+      "jdbc:sqlite:C:/sqlite/db/chinook.db",
+      "src/main/scala",
+      "org.opal.db.model",
+      "chinook",
+      "test",
+    )
+  )
+}
--- src/main/scala/UsingSlick.scala
+++ src/main/scala/UsingSlick.scala
@@ -1,24 +1,39 @@
-import scala.concurrent.Await
+import org.opal.db.model.Tables
 
-object UsingSlick extends App {
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+object UsingSlick extends App{
   println("Trying Slick")
   import slick.jdbc.SQLiteProfile.api._ // must import
-  val db = Database.forURL(url = "c:/sqlite/db/chinook.db")
-//  lazy val db = Database.forURL(url = "c:/sqlite/db/chinook.db") with lazy the function is not called until needed
+val db = Database.forURL(url = "jdbc:sqlite:c:\\sqlite\\db\\chinook.db", driver="org.sqlite.JDBC")
+  val tables = Tables
+  val albums = tables.Albums
+  val genres = tables.Genres
+  //  println(genres.toString())
+//  db.run(genres.result).map(res => res.foreach {
+//    case (Genres.) =>
+//      println("something")
+//  })
+//  })
+  val q = genres.map(_.name) // so this creates an SQL query out of scala code an example of ORM
+  val action = q.result
+  val result: Future[Seq[Option[String]]] = db.run(action)
+  val sql = action.statements.head
+  println(sql)
+  Thread.sleep(1500) //shouldnt be needed
+  println("Awoke now")
+  var myVector: Seq[Option[String]] = Seq(Option("Aha"))
+  result.onComplete {
+    case Success(s) => {
+      println(s"Result: $s")
 
-//  val db = Database.forURL(url = "c:/sqlite/db/chinookbad.db")
-//  import scala.concurrent.ExecutionContext.Implicits.global
+    }
+    case Failure(t) => t.printStackTrace()
+    case _ => println("Unknown happened")
+  }
+  myVector.foreach(println)
 
-  //so this code generator should run whenever we change our Database schema
-  slick.codegen.SourceCodeGenerator.main(
-    Array(
-      "slick.jdbc.SQLiteProfile",
-      "org.sqlite.JDBC",
-      "jdbc:sqlite:C:/sqlite/db/chinook.db",
-      "src/main/scala",
-      "org.opal.db.model",
-      "chinook",
-      "test",
-    )
-  )
+//  result.foreach(println)
 }
