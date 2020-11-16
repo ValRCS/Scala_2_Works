e553db872fff03f9dc8c61c1993db59d79b77d60
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-11-16 21:31:55 +0200

    Redis with enviroment variables

--- src/main/scala/RedisConn.scala
+++ src/main/scala/RedisConn.scala
@@ -2,13 +2,13 @@ import com.redis.RedisClient
 
 object RedisConn extends App{
   println("Connecting to Redis")
-  //this should be read from .env file really
-  val host = "myhost"
+  val host = scala.util.Properties.envOrElse("REDISHOST", "undefined") //of course care must be taken for name colission
+  println(s"My host is $host")
   val port = 10283
   val db = "FunStoreNov20"
-  val mysecret = "secret" //FIXME ASAP move to .env
+  val secret = scala.util.Properties.envOrElse("REDISPW", "nosuchsecret")
 
-  val r = new RedisClient(host, port, secret = mysecret)
+  val r = new RedisClient(host, port, 0, Some(secret))
   r.set("name", "Valdis")
   println(r.get("name"))
 }
