0ba73b16ae8e823857d62b74ea2a80000429654d
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-11-21 14:54:09 +0200

    Extracting data from Neo4J into case class collection

--- src/main/scala/UsingNeo4Java.scala
+++ src/main/scala/UsingNeo4Java.scala
@@ -4,6 +4,8 @@ import java.util
 import java.util.Arrays.asList
 import java.util.Collections.singletonMap
 
+import scala.collection.mutable.ArrayBuffer
+
 
 object UsingNeo4Java extends App {
 
@@ -14,23 +16,35 @@ object UsingNeo4Java extends App {
 //    val noSSL = Config.builder.withEncryptionLevel(Config.EncryptionLevel.NONE).toConfig
     val noSSL = Config.builder().build() //TODO possible need to set Encryption Level to None
 
+    final case class Movie(id: Int, title: String, tagline: String,  released: Int)
 
-    val driver = GraphDatabase.driver("bolt://52.4.89.242:37125", AuthTokens.basic("neo4j", "changme"), noSSL) // <password>
+    val driver = GraphDatabase.driver("bolt://52.4.89.242:37125", AuthTokens.basic("neo4j", "notthepw"), noSSL) // <password>
+    val arrayBuffer1: ArrayBuffer[Movie] = ArrayBuffer()
     try {
       val session = driver.session
+
       try {
 //        val cypherQuery = "MATCH (n) " + "RETURN id(n) AS id"
-        val cypherQuery = "MATCH (m:Movie) " + "RETURN m.title AS title, id(m) as id"
+//        val cypherQuery = "MATCH (m:Movie) " + "RETURN m.title AS title, id(m) as id"
+        val cypherQuery = "MATCH (m:Movie) " + "RETURN m as movie, id(m) as id"
         val result = session.run(cypherQuery, parameters()) //TODO check if parameters need values
+
         while ( {
           result.hasNext
         }) {
           val record = result.next // so somewhat similar to row in SQL one entry
 //          System.out.println(result.next.get("id"))
-          println(record.get("id"), record.get("title"))
+//          println(record.get("id"), record.get("title"))
+          val movie = record.get("movie")
+          println(record.get("id"),movie.get("title"),movie.get("tagline"),movie.get("released"))
+          arrayBuffer1 += Movie(record.get("id").asInt(),movie.get("title").asString(),movie.get("tagline").asString(),movie.get("released").asInt())
+          //this is where you would store it in a case class type callin
         }
       } finally if (session != null) session.close()
     }
+    driver.close()
+    val movies = arrayBuffer1.toArray
+    movies.filter(_.released > 2005).foreach(println)
   }
   main()
 }
